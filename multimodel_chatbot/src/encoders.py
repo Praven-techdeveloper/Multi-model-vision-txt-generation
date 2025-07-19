44cdd439635ea7c1b1eada4dac1a63761c1a26d6
Praven-Tech-Developer <praven4405@gmail.com>
2025-07-20 00:35:39 +0530

    Update encoders.py

--- multimodel_chatbot/src/encoders.py
+++ multimodel_chatbot/src/encoders.py
@@ -1,13 +1,10 @@
 from transformers import CLIPProcessor, CLIPModel
-
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-
 def embed_image(image):
     inputs = processor(images=image, return_tensors="pt")
     feats = clip_model.get_image_features(**inputs)
     return feats.detach().cpu().numpy()
-
 def embed_text(texts: list):
     inputs = processor(text=texts, return_tensors="pt",
                        padding=True, truncation=True)
